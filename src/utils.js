4a9576e3d85f140bf091537010927fab4e2fcbfe
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2018-04-05 11:40:17 -0700

    Create functions to get the content of every cell in the grid

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,9 @@
-const configPerLevel = {
+const imgMine = require('./img/minesweeper_mine.png');
+const img1 = require('./img/minesweeper_1.png');
+const img2 = require('./img/minesweeper_2.png');
+const img3 = require('./img/minesweeper_3.png');
+
+const configForLevel = {
   1: {
     mines: 10,
     width: 9,
@@ -17,7 +22,7 @@ const configPerLevel = {
 };
 
 export function getConfigForLevel(level) {
-  return configPerLevel[level];
+  return configForLevel[level];
 }
 
 export function getNumberOfMines(level) {
@@ -50,4 +55,73 @@ export function getMinesCoordinates(level) {
   }
 
   return mines;
+}
+
+export function isMine(mines, cell) {
+  for (let i = 0; i < mines.length; i += 1) {
+    const mine = mines[i];
+    if (mine[0] === cell[0] && mine[1] === cell[1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function getCellValue(level, mines, cell) {
+  const width = getWidth(level);
+  const height = getHeight(level);
+
+  if (isMine(mines, cell)) {
+    return '*';
+  }
+
+  const cellStartRow = Math.max(0, cell[0] - 1);
+  const cellEndRow = Math.min(height, cell[0] + 1);
+  const cellStartColumn = Math.max(0, cell[1] - 1);
+  const cellEndColumn = Math.min(width, cell[1] + 1);
+
+  let result = 0;
+
+  for (let row = cellStartRow; row <= cellEndRow; row += 1) {
+    for (let col = cellStartColumn; col <= cellEndColumn; col += 1) {
+      if (isMine(mines, [row, col])) {
+        result += 1;
+      }
+    }
+  }
+  return result.toString();
+}
+
+export function getGrid(level, mines) {
+  const width = getWidth(level);
+  const height = getHeight(level);
+
+  const grid = [];
+
+  for (let row = 0; row < height; row += 1) {
+    const currentRow = [];
+    for (let col = 0; col < width; col += 1) {
+      currentRow.push(getCellValue(level, mines, [row, col]));
+    }
+    grid.push(currentRow);
+  }
+
+  return grid;
+}
+
+export function getCellImage(content) {
+  if (content === '*') {
+    return imgMine;
+  }
+  if (content === '1') {
+    return img1;
+  }
+  if (content === '2') {
+    return img2;
+  }
+  if (content === '3') {
+    return img3;
+  }
+
+  return '';
 }
--- src/utils.test.js
+++ src/utils.test.js
@@ -5,6 +5,10 @@ import {
   getHeight,
   getRandomMine,
   getMinesCoordinates,
+  isMine,
+  getCellValue,
+  getGrid,
+  getCellImage,
 } from './utils';
 
 describe('utils', () => {
@@ -64,6 +68,71 @@ describe('utils', () => {
 
     it('returns an array with the coordinates of all the mines', () => {
       expect(getMinesCoordinates(level)).toHaveLength(10);
+    });
+  });
+
+  describe('isMine', () => {
+    const mines = [[0, 2], [1, 2]];
+
+    it('returns an array with the content of every cell in the grid', () => {
+      const cell = [0, 2];
+      expect(isMine(mines, cell)).toEqual(true);
+    });
+
+    it('returns an array with the content of every cell in the grid', () => {
+      const cell = [2, 2];
+      expect(isMine(mines, cell)).toEqual(false);
+    });
+  });
+
+  describe('getCellValue', () => {
+    const level = 1;
+    const mines = [[0, 2], [1, 2]];
+
+    it('returns a string with the content of the selected cell', () => {
+      const cell = [1, 1];
+      expect(getCellValue(level, mines, cell)).toEqual('2');
+    });
+
+    it('returns a string "*" if the selected cell is a mine', () => {
+      const cell = [0, 2];
+      expect(getCellValue(level, mines, cell)).toEqual('*');
+    });
+  });
+
+  describe('getGrid', () => {
+    const level = 1;
+    const mines = [
+      [0, 0], [0, 5], [1, 3], [2, 8], [3, 0], [3, 6], [5, 5], [7, 3], [8, 7], [8, 8]];
+    const result = [
+      ['*', '1', '1', '1', '2', '*', '1', '0', '0'],
+      ['1', '1', '1', '*', '2', '1', '1', '1', '1'],
+      ['1', '1', '1', '1', '1', '1', '1', '2', '*'],
+      ['*', '1', '0', '0', '0', '1', '*', '2', '1'],
+      ['1', '1', '0', '0', '1', '2', '2', '1', '0'],
+      ['0', '0', '0', '0', '1', '*', '1', '0', '0'],
+      ['0', '0', '1', '1', '2', '1', '1', '0', '0'],
+      ['0', '0', '1', '*', '1', '0', '1', '2', '2'],
+      ['0', '0', '1', '1', '1', '0', '1', '*', '*']];
+
+    it('returns an array with the content of every cell in the grid', () => {
+      expect(getGrid(level, mines)).toEqual(result);
+    });
+
+    it('returns the length of the grid', () => {
+      expect(getGrid(level, mines)).toHaveLength(9);
+    });
+  });
+
+  describe('getCellImage', () => {
+    it('returns the image of a mine if the selected cell is a mine', () => {
+      const content = '2';
+      expect(getCellImage(content)).toEqual('minesweeper_2.png');
+    });
+
+    it('returns the image of the number of mines around the selected cell if the selected cell is not a mine', () => {
+      const content = '*';
+      expect(getCellImage(content)).toEqual('minesweeper_mine.png');
     });
   });
 });
