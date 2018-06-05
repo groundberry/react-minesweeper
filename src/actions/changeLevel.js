fafd61147292239db69032be447cacc1d1908caf
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2018-06-05 16:12:55 -0700

    Fix error when changing level

--- src/actions/changeLevel.js
+++ src/actions/changeLevel.js
@@ -5,6 +5,10 @@ export default function changeLevel(level) {
     level,
     grid: startGame(level),
     pressedGrid: getPressedGrid(level),
+    flaggedGrid: getPressedGrid(level),
     gameOver: false,
+    winGame: false,
+    startTime: null,
+    endTime: null,
   };
 }
